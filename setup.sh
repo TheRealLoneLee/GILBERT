90a9c8c65e7a8c117fadf5a8e35c2f2069238c91
Lee <thereallonelee@outlook.com>
2023-09-11 18:54:09 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,8 @@
 sudo apt update # Update the system
 sudo apt install python3 python3-pip # Install Python 3 and pip
 pip3 install pycord # Install the pycord library
+pip3 install python-dotenv # Install the dotenv library
+pip3 install twitchio # Install the twitchio library
+pip3 install aiohttp # Install the aiohttp library
+
+python3 database_setup.py # Setup the database
