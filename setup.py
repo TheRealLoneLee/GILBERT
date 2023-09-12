1525a7c28f991aa0a0fbdb86fe7dd4d12c773b27
Lee-projecthelios <thereallonelee@outlook.com>
2023-09-12 15:56:39 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,6 +31,6 @@ os.system('chmod +x ./initiate.sh')
 # Ask the user if they want to initiate the bot
 initiate_bot = input("Do you wish to initiate the bot? (Y/N) ")
 if initiate_bot.lower() == 'y':
-    os.system('/initiate.sh')
+    os.system('./initiate.sh')
 else:
     print("Understood. Skipping this step. (If you wish to initiate the bot run the initiate.sh script manually.)")
