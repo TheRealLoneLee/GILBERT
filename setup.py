5e0d59fe0691e71968e3173f26f0f1a25c220aae
Lee-projecthelios <thereallonelee@outlook.com>
2023-09-12 15:19:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,11 +26,11 @@ with open('bot/config.json', 'w') as f:
     json.dump(config, f)
 
 # Make the initiate.sh script executable
-os.system('chmod +x bot/initiate.sh')
+os.system('chmod +x ./initiate.sh')
 
 # Ask the user if they want to initiate the bot
 initiate_bot = input("Do you wish to initiate the bot? (Y/N) ")
 if initiate_bot.lower() == 'y':
-    os.system('cd bot && ./initiate.sh')
+    os.system('./initiate.sh')
 else:
     print("Understood. Skipping this step. (If you wish to initiate the bot run the initiate.sh script manually.)")
