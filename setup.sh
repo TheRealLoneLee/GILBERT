c70fbcf6e5890f7487af6f699641ea60fd8cd879
Lee-projecthelios <thereallonelee@outlook.com>
2023-09-12 14:46:42 +0100

    Update setup.sh
    Updated the setup.sh script for automatic installation and setup of the bot on the test/dev environment.

--- setup.sh
+++ setup.sh
@@ -1,12 +1,71 @@
 #!/bin/bash
-# This script is used to setup the environment for the Gilbert Discord Bot on PI 4
-sudo apt update # Update the system
-sudo apt install python3 python3-pip # Install Python 3 and pip
-pip3 install pycord # Install the pycord library
+# This script is used to setup the environment for the Gilbert Discord Bot on a Raspberry Model PI 4 4GB
+# running Raspbian 10 64-bit. It will check for updates to the bot, Python, and pip. It will also install
+# the Pycord, dotenv, twitchio, and aiohttp libraries. Finally, it will prompt the user to run the
+# database setup and start the bot.
+
+# Create the directory if it doesn't exist
+if [ ! -d "bot" ]; then
+    mkdir bot
+fi
+
+# Navigate to your bot directory
+cd bot
+
+# Check for updates from the remote repository
+git remote update
+
+# If updates were found, pull them
+if [ $(git status -uno | grep 'Your branch is behind' | wc -l) -gt 0 ]; then
+    echo "Updates found. Pulling latest changes..."
+    git pull
+else
+    echo "No updates found."
+fi
+
+# Check if Python is installed and update it if necessary
+if command -v python3 &>/dev/null; then
+    echo "Python 3 is installed. Checking for updates..."
+    sudo apt update && sudo apt upgrade python3
+else
+    echo "Python 3 is not installed. Installing..."
+    sudo apt update && sudo apt install python3
+fi
+
+# Check if pip is installed and update it if necessary
+if command -v pip3 &>/dev/null; then
+    echo "pip is installed. Checking for updates..."
+    pip3 install --upgrade pip
+else
+    echo "pip is not installed. Installing..."
+    sudo apt install python3-pip
+fi
+
+pip3 install pycord # Install the Pycord library
 pip3 install python-dotenv # Install the dotenv library
 pip3 install twitchio # Install the twitchio library
 pip3 install aiohttp # Install the aiohttp library
 
-python3 database_setup.py # Setup the database
+# Prompt the user before running the database setup
+read -p "Do you want to run the database setup? (Y/N) " answer
+case ${answer:0:1} in
+    y|Y )
+        echo "Running database setup..."
+        python3 database_setup.py # Setup the database
+    ;;
+    * )
+        echo "Skipping database setup."
+    ;;
+esac
 
-python3 main.py # Run the bot
+# Prompt the user before starting the bot
+read -p "Do you want to start the bot? (Y/N) " answer
+case ${answer:0:1} in
+    y|Y )
+        echo "Starting the bot..."
+        python3 main.py # Run the bot
+    ;;
+    * )
+        echo "Skipping automatic bot start. (You can start the bot manually by running 'python3 main.py') Enjoy!"
+    ;;
+esac
