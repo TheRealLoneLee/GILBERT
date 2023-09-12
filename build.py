72865302b855a58f9216cbde93b652204e82a418
Lee-projecthelios <thereallonelee@outlook.com>
2023-09-12 15:28:46 +0100

    Create build.py
    Simple script to speed up the build process for creating automatic install scripts for dev/test purposes.

---
+++ build.py
@@ -0,0 +1,28 @@
+import os
+import zipfile
+import shutil
+from tqdm import tqdm
+
+# Check if the directory "builds" exists
+if not os.path.exists("builds"):
+    os.makedirs("builds")
+
+# Ask the user for the current version
+version = input("Please enter the current version in the format 'v0.0.0': ")
+
+# Define the .zip file name
+zipfile_name = "Gilbert-" + version + ".zip"
+
+# Create a ZipFile object
+with zipfile.ZipFile(os.path.join("builds", zipfile_name), 'w') as zipf:
+    # List of files to add
+    files_to_add = ["./setup.py", "./initiate.sh"]
+
+    # Iterate over each file
+    for file in tqdm(files_to_add, desc="Zipping files", unit="file"):
+        # Check if the file exists
+        if os.path.exists(file):
+            # Add file to the .zip file
+            zipf.write(file)
+        else:
+            print(f"{file} does not exist in the current directory.")
