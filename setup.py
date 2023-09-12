3d70c0aeb77456470af9826ab985a0190a0e856f
Lee-projecthelios <thereallonelee@outlook.com>
2023-09-12 15:14:09 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -25,9 +25,12 @@ if twitch_integration.lower() == 'y':
 with open('bot/config.json', 'w') as f:
     json.dump(config, f)
 
+# Make the initiate.sh script executable
+os.system('chmod +x bot/initiate.sh')
+
 # Ask the user if they want to initiate the bot
 initiate_bot = input("Do you wish to initiate the bot? (Y/N) ")
 if initiate_bot.lower() == 'y':
-    os.system('cd bot && ./startup.sh')
+    os.system('cd bot && ./initiate.sh')
 else:
-    print("Understood. Skipping this step. (If you wish to initiate the bot run the startup.sh script manually.)")
+    print("Understood. Skipping this step. (If you wish to initiate the bot run the initiate.sh script manually.)")
